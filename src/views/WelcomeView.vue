b38f468ab7fa6445e482c7af795884ee6f45c4d9
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-07-28 17:42:57 +0800

    Update WelcomeView.vue

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,90 +1,87 @@
 <script setup>
-    import { RouterView, RouterLink } from 'vue-router';
+import { RouterView, RouterLink } from "vue-router";
 </script>
 
-
 <template>
-    <div class="container">
-        <div class="content">
-            <h2>Kaihzan-Nixxo Jamilla</h2>
-            <!-- button the links to /home -->
-            <RouterLink to="/home" class="button">
-                Go to app
-            </RouterLink>
-
-        </div>
+  <div class="container">
+    <div class="content">
+      <h2>Kaihzan-Nixxo Jamilla</h2>
+      <!-- button the links to /home -->
+      <RouterLink to="/home" class="button"> Go to app </RouterLink>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-    .container {
-        width: 100%;
-        height: 100vh;
-        background: #f5f5f5;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        animation: fadeIn 1s ease-in-out;
-        
-    }
+.container {
+  width: 100%;
+  height: 100vh;
+  background: #f5f5f5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-    @keyframes fadeIn {
-        0% {
-            opacity: 0;
-        }
+.content {
+  width: 100%;
+  max-width: 500px;
+  height: 300px;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  animation: fadeIn 0.9s ease-in-out;
+}
 
-        100% {
-            opacity: 1;
-        }
-    }
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+    transform: translateY(90px);
+  }
 
-    .content {
-        width: 100%;
-        max-width: 500px;
-        height: 300px;
-        background: #fff;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        padding: 20px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
 
-    .content h2 {
-        font-size: 30px;
-        font-weight: 500;
-        margin-bottom: 20px;
-    }
+.content h2 {
+  font-size: 30px;
+  font-weight: 500;
+  margin-bottom: 20px;
+}
 
-    .content .button {
-        width: 100%;
-        max-width: 200px;
-        height: 50px;
-        background: #000;
-        color: #fff;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: 0.3s;
-    }
+.content .button {
+  width: 100%;
+  max-width: 200px;
+  height: 50px;
+  background: #000;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.3s;
+}
 
-    .content .button:hover {
-        background: #333;
-    }
+.content .button:hover {
+  background: #333;
+}
 
-    .content .button:active {
-        background: #555;
-    }
+.content .button:active {
+  background: #555;
+}
 
-    .content .button:focus {
-        outline: none;
-    }
-    .content .button span {
-        font-size: 20px;
-        font-weight: 500;
-    }
+.content .button:focus {
+  outline: none;
+}
+.content .button span {
+  font-size: 20px;
+  font-weight: 500;
+}
 </style>
